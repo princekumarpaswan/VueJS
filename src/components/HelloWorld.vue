953b11587dc54213fef04464618351afdf064a9e
princekumarpaswan <prasantandal123@gmail.com>
2024-07-12 21:34:21 +0530

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script>
+console.log('hehehe')
+</script>
+
+
+<template> 
+<h1 class="he" >Hello new</h1>
+ </template>
+
+ <style>
+ h1{
+    color: red;
+ }
+ .he{
+    color: green;
+ }
+</style>
